f223f19b2633198f8ba8f07fd3a31b02df8f65a9
Ivan Petrukhin <monkpatch@gmail.com>
2022-11-12 01:40:07 +0300

    Create command for generating tasks

--- Class1/Task1/Program.cs
+++ Class1/Task1/Program.cs
@@ -7,16 +7,46 @@ public class Program
         switch (args)
         {
             case ["table", var n]:
+            {
                 TablePrinter.PrintTable(int.Parse(n));
                 break;
+            }
             case ["vulgar", var input, var filename]:
+            {
                 var r = new Rational(input);
                 var output = VulgarFractions.ToMixedVulgarFraction(r);
                 File.WriteAllText(filename, output);
                 break;
-            default:
-                Console.WriteLine("Usage: Task1.exe command rational filename");
+            }
+            case ["generate", var number, var filename]:
+            {
+                using var file = new StreamWriter(filename);
+                for (var i = 0; i < int.Parse(number); i++)
+                {
+                    var numerator1 = 1 + Random.Shared.Next() % 99;
+                    var denominator1 = 1 + Random.Shared.Next() % 99;
+                    var rational1 = new Rational(numerator1, denominator1);
+                    
+                    var numerator2 = 1 + Random.Shared.Next() % 99;
+                    var denominator2 = 1 + Random.Shared.Next() % 99;
+                    var rational2 = new Rational(numerator2, denominator2);
+                    
+                    var rational3 = rational1 + rational2;
+
+                    var str1 = VulgarFractions.ToMixedVulgarFraction(rational1);
+                    var str2 = VulgarFractions.ToMixedVulgarFraction(rational2);
+                    var str3 = VulgarFractions.ToMixedVulgarFraction(rational3);
+                    
+                    file.WriteLine($"{str1}+{str2}={str3}");
+                }
+
                 break;
+            }
+            default:
+            {
+                Console.WriteLine("Incorrect usage");
+                break;
+            }
         }
     }
 }
