fa48f923647bf7786c0df013c473e62a726731b6
Ivan Petrukhin <monkpatch@gmail.com>
2022-11-12 01:46:30 +0300

    Improve task-generator

--- Class1/Task1/Program.cs
+++ Class1/Task1/Program.cs
@@ -26,22 +26,24 @@ public class Program
                     var numerator1 = 1 + Random.Shared.Next() % 99;
                     var denominator1 = 1 + Random.Shared.Next() % 99;
                     var rational1 = new Rational(numerator1, denominator1);
-                    
+
                     var numerator2 = 1 + Random.Shared.Next() % 99;
                     var denominator2 = 1 + Random.Shared.Next() % 99;
                     var rational2 = new Rational(numerator2, denominator2);
-                    
+
                     var rational3 = rational1 + rational2;
 
                     var str1 = VulgarFractions.ToMixedVulgarFraction(rational1);
                     var str2 = VulgarFractions.ToMixedVulgarFraction(rational2);
                     var str3 = VulgarFractions.ToMixedVulgarFraction(rational3);
-                    
-                    file.WriteLine($"{str1}+{str2}={str3}");
+                    file.WriteLine(Random.Shared.Next() % 2 == 0
+                        ? $"{str1} + ?? = {str3} # ({str2})"
+                        : $"{str1} + {str2} = ?? # ({str3})");
                 }
 
                 break;
             }
+
             default:
             {
                 Console.WriteLine("Incorrect usage");
